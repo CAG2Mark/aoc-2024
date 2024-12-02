98f9c7f015d945e9018c56b8a55211ae859148c5
CAG2Mark <markng989@gmail.com>
2024-12-02 13:42:14 +0800

    smarter day 2

--- day02/p2.py
+++ day02/p2.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# ORIGINAL SLOW SOLUTION
 def safe(s):
     diff = []
     for i in range(len(s) - 1):
@@ -19,8 +20,38 @@ def has_safe(ln):
             return True
     return False
 
+# SMARTER SOLUTION
+def has_safe_smart(ln):
+    s = [int(x) for x in ln.split()]
+    diff = []
+    for i in range(len(s) - 1):
+        diff.append(s[i + 1] - s[i])
+    
+    neg = sum([1 for x in diff if x < 0])
+    if neg >= len(diff) - 2:
+        diff = [-x for x in diff]
+    
+    fixed = 0
+    for i, d in enumerate(diff):
+        if 1 <= d <= 3: continue
+        if fixed >= 1: return False
+        
+        fixed += 1
+        # IMPORTANT: always try to combine with the later step (i.e. delete the later level) first!
+        # if the previous steps were already fine, only combine them if needed 
+        if i < len(diff) - 1 and 1 <= d + diff[i + 1] <= 3: # delete level in front
+            diff[i + 1] += d
+        elif i > 0 and 1 <= diff[i - 1] + d <= 3: # delete level behind
+            pass
+        elif i == len(diff) - 1 or i == 0: # if at either endpoint, nothing we can do but delete the current level
+            pass
+        else:
+            return False
+    return True
+
+
 def solve(inp: List[str]):
     ans = 0
     for ln in inp:
-        ans += has_safe(ln)
+        ans += has_safe_smart(ln)
     return ans
