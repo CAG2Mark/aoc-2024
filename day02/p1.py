2627991969ae54a06e9ae8515db86f289346e5fa
CAG2Mark <markng989@gmail.com>
2024-12-02 13:19:56 +0800

    day 2

--- day02/p1.py
+++ day02/p1.py
@@ -1,4 +1,14 @@
 from typing import List
 
 def solve(inp: List[str]):
-    return None
+    ans = 0
+    for ln in inp:
+        s = [int(x) for x in ln.split()]
+        diff = []
+        for i in range(len(s) - 1):
+            diff.append(s[i + 1] - s[i])
+        if diff[0] < 0: 
+            diff = [-x for x in diff]
+            
+        ans += all([1 <= d <= 3 for d in diff])
+    return ans
--- day02/p2.py
+++ day02/p2.py
@@ -1,4 +1,26 @@
 from typing import List
 
+def safe(s):
+    diff = []
+    for i in range(len(s) - 1):
+        diff.append(s[i + 1] - s[i])
+    if diff[0] < 0: 
+        diff = [-x for x in diff]
+        
+    return all([1 <= d <= 3 for d in diff])
+
+def has_safe(ln):
+    s = [int(x) for x in ln.split()]
+    # lazy solution, just guess and check which one to delete :P
+    for i in range(len(s)):
+        c = s.copy()
+        del c[i]
+        if safe(c):
+            return True
+    return False
+
 def solve(inp: List[str]):
-    return None
+    ans = 0
+    for ln in inp:
+        ans += has_safe(ln)
+    return ans
